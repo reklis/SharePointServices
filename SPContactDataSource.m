0f43a36aa9e24348c8280eeae0284b01c8898ef4
Steven Fusco <sfusco@cibotechnology.com>
2011-03-12 13:31:27 -0500

    returning full alphabet instead of just contact section headings (like Contacts.app)

--- SPContactDataSource.m
+++ SPContactDataSource.m
@@ -155,7 +155,7 @@
 
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView
 {
-    return self.contactIndexes;
+    return [NSArray arrayWithObjects:@"A", @"B", @"C", @"D", @"E", @"F", @"G", @"H", @"I", @"J", @"K", @"L", @"M", @"N", @"O", @"P", @"Q", @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z", nil];
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
